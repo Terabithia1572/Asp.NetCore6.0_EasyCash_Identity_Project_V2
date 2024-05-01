ec79ce02569732358a01b5fde42b340802574c9a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-01 18:44:34 +0300

    Referance Process + Writing Entities

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProccess
+    {
+        public int CustomerAccountProccessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+    }
+}
