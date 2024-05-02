6e7b229abf6cd06ae516f24a89fb124ae828c336
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-02 22:18:52 +0300

    AppUser+ AppRole Tables Added + Migration Edit / Added

--- EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Concrete
 {
-    public class Context:IdentityDbContext
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -17,5 +17,6 @@ namespace EasyCashIdentityProject.DataAccessLayer.Concrete
         }
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProccess> CustomerAccountProccesses { get; set; }
+        //Appuser için Dbset oluşturmamuza gerek yok
     }
 }
--- EasyCashIdentityProject.DataAccessLayer/Migrations/20240502191743_mig_addd_AppUser_AppRole_Tables.Designer.cs
+++ EasyCashIdentityProject.DataAccessLayer/Migrations/20240502191743_mig_addd_AppUser_AppRole_Tables.Designer.cs
@@ -4,6 +4,7 @@ using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EasyCashIdentityProject.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240502191743_mig_addd_AppUser_AppRole_Tables")]
+    partial class mig_addd_AppUser_AppRole_Tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
