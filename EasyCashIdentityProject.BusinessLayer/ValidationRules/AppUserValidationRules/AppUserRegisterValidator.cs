673b30a8704498ab1a3c53082a7c2bc82210a524
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-02 23:40:49 +0300

    AppUser Validation Process Added

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,28 @@
+﻿using EasyCashIdentityProject.DTOLayer.DTOS.AppUserDTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDTO>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x=>x.Name).NotEmpty().WithMessage("İsim Alanı Boş Geçilemez..");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad Kısmı Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre Kısmı Boş Geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar Kısmı Boş Geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email Kısmı Boş Geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı Adı Kısmı Boş Geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Lütfen En Fazla 30 Karakter Girişi Yapınız..");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapınız..");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız Eşleşmiyor..");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen Geçerli Bir Mail Adresi Giriniz..");
+
+        }
+    }
+}
