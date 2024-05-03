ee8b76c583dd34da93aca801ebb902f7992762fd
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-03 19:41:51 +0300

    Writing Generic Repository & Interface Process Added

---
+++ EasyCashIdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new();
+        public void Delete(T t)
+        {
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+        }
+    }
+}
