366885aa4e8856101a48d6190a689cc9f73c819f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-05 21:12:21 +0300

    Generic Process Added + Theme files Added

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProccess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public CustomerAccountProccess TGetByID(int id)
+        {
+            return _customerAccountProcessDal.GetByID(id);  
+        }
+
+        public List<CustomerAccountProccess> TGetList()
+        {
+            return _customerAccountProcessDal.GetList();
+        }
+
+        public void TInsert(CustomerAccountProccess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProccess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
