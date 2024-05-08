252d46d807eb04d214bb892375ec2016ab683595
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-08 22:16:11 +0300

    Confirm Mail Process Continue

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+	public class ConfirmMailController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ EasyCashIdentityProject.PresentationLayer/Models/ConfirmMailViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class ConfirmMailViewModel
+    {
+        public int ID { get; set; }
+        public int ConfirmCode { get; set; }
+    }
+}
