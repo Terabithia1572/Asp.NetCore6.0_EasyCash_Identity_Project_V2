f42381ca135c446e5c5988ea710460c573b76712
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-05-16 23:32:20 +0300

    MyAccountController Updated + fetch logged in user information Added + MyAccounts Page Updated

---
+++ EasyCashIdentityProject.DTOLayer/DTOS/AppUserDTOs/AppUserEditDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DTOLayer.DTOS.AppUserDTOs
+{
+    public class AppUserEditDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string ImageURL { get; set; }
+        public string EMail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyCashIdentityProject.DTOLayer.DTOS.AppUserDTOs;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
+    [Authorize]
     public class MyAccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public MyAccountsController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserEditDTO appUserEditDTO = new();
+            appUserEditDTO.Name = values.CustomerName;
+            appUserEditDTO.Surname = values.CustomerSurname;
+            appUserEditDTO.ImageURL = values.ImageURL;
+            appUserEditDTO.PhoneNumber=values.PhoneNumber; 
+            appUserEditDTO.District= values.District;
+            appUserEditDTO.City= values.City;
+            appUserEditDTO.EMail = values.Email;
+
+            return View(appUserEditDTO);
+        }
+        //[HttpPost]
     }
 }
