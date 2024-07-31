9c758b95ca0d781da631c61cf1f8fba5796a60da
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-07-31 23:17:45 +0300

    Data Transfer Operations Added via Sidebar

--- EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneyController.cs
@@ -20,8 +20,9 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string mycurrency)
         {
+            ViewBag.currency = mycurrency;
             return View();
         }
         [HttpPost]
