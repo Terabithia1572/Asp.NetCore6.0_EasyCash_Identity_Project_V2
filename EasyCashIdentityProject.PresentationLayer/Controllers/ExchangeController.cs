d04c4c7b9f4cb5b7c9b27d950dd15d6042865377
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-01 23:48:21 +0300

    Exchange Currency Page Added + Retrieving Exchange Rate Information with Rapid API Process + Added

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            //using System.Net.Http.Headers;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ViewBag.Exchange = body;
+            }
+            return View();
+        }
+    }
+}
