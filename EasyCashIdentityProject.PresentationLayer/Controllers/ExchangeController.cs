a119b72684d9a18b9e85b768dcd23b2a71037cca
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-01 23:55:18 +0300

    Retrieving Exchange Rate Information with Rapid API Updated Exchange Controller

--- EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ExchangeController.cs
@@ -1,28 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
     public class ExchangeController : Controller
     {
         public async Task<IActionResult> Index()
         {
-            //using System.Net.Http.Headers;
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=TRY&amount=1"),
                 Headers =
-    {
-        { "x-rapidapi-key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
-        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
-    },
+        {
+            { "x-rapidapi-key", "44a9b3ca28msh4dc1bc7bb828d5ap18c256jsn9125f1e0404c" },
+            { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+        },
             };
+
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
-                ViewBag.Exchange = body;
+
+                // JSON içeriğini ayrıştır
+                var jsonResponse = JObject.Parse(body);
+
+                // result kısmını al
+                var result = jsonResponse["result"].ToString();
+
+                // ViewBag'e result değerini ekle
+                ViewBag.Exchange = result;
             }
             return View();
         }
