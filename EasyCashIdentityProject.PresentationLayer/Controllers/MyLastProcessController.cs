c5883a67e3d2a1d5ad8bcd7cd24e34cee8a27a38
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2024-08-03 23:19:43 +0300

    My Last Process Process Updated + Index Page Updated

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyLastProcessController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyLastProcessController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
     public class MyLastProcessController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountProcessService _customerAccountProcessService;
+
+        public MyLastProcessController(UserManager<AppUser> userManager, ICustomerAccountProcessService customerAccountProcessService)
         {
-            return View();
+            _userManager = userManager;
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var values = _customerAccountProcessService.TMyLastProcess(user.Id);
+            return View(values);
         }
     }
 }
